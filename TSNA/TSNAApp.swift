dc6fd9d5d08a317356dbb154a2fd198dd2f36d2a
sim <cscaesim@gmail.com>
2021-06-17 16:05:56 -0500

    Initial Commit

---
+++ TSNA/TSNAApp.swift
@@ -0,0 +1,17 @@
+//
+//  TSNAApp.swift
+//  TSNA
+//
+//  Created by Caine Simpson on 6/17/21.
+//
+
+import SwiftUI
+
+@main
+struct TSNAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
